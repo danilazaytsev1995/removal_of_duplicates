b8b17f5c53e785384f0b8e679e27d3373a16a9d9
danilazaytsev1995 <indie.hare@gmail.com>
2021-11-23 16:14:57 +0300

    че то сделал v0.001337

--- src/Solution.java
+++ src/Solution.java
@@ -8,7 +8,7 @@ public class Solution {
         String dat = ".dat";
         String file_path = "E:/Danila/files_copy/full_after_cleaning"; // вставить название исходного файла
         File input_file = new File(file_path + dat); // входной файл
-        File output_file = new File(file_path + "_bez_dublerov" + dat); // получаемый файл
+        File output_file = new File(file_path + "_without_double" + dat); // получаемый файл
         try(BufferedReader reader = new BufferedReader(new FileReader(input_file));
             BufferedWriter writer = new BufferedWriter(new FileWriter(output_file))) {
             while (reader.ready()) {
