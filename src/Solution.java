0640254ffa04fdfe8f9be54ba1b067f02a2235a7
danilazaytsev1995 <indie.hare@gmail.com>
2021-11-23 14:11:16 +0300

    подправил маленько

--- src/Solution.java
+++ src/Solution.java
@@ -6,23 +6,18 @@ public class Solution {
     public static void main(String[] args) {
         Set<String> lines = new HashSet<>();
         String dat = ".dat";
-        String file_path = "E:/Danila/files_copy/full_after_cleaning";
-        try {
-            File input_file = new File(file_path + dat); // вставить название входного файла
-            File output_file = new File(file_path + "_bez_dublerov" + dat); // вставить название получаемого файла
-            BufferedReader reader = new BufferedReader(new FileReader(input_file));
-            BufferedWriter writer = new BufferedWriter(new FileWriter(output_file));
+        String file_path = "E:/Danila/files_copy/full_after_cleaning"; // вставить название исходного файла
+        File input_file = new File(file_path + dat); // входной файл
+        File output_file = new File(file_path + "_bez_dublerov" + dat); // получаемый файл
+        try(BufferedReader reader = new BufferedReader(new FileReader(input_file));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(output_file))) {
             while (reader.ready()) {
                 lines.add(reader.readLine());
             }
-            reader.close(); //закрыл ридер
             for (String line : lines) {
                 writer.write(line);
                 writer.newLine();
             }
-            writer.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
